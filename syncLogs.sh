e0ad06acdf525610d62ea00713ab0e5820a43c67
Benjamin Cabé <benjamin@eclipse.org>
2014-11-28 20:09:47 +0100

    use rsync

--- syncLogs.sh
+++ syncLogs.sh
@@ -1,2 +1,2 @@
-scp root@iot.eclipse.org:~/logs/screen-0S-0.log ./lwm2m.log
-scp root@iot.eclipse.org:~/logs/screen-1S-1.log ./cf.log
+rsync --progress --compress root@iot.eclipse.org:~/logs/screen-0S-0.log ./lwm2m.log
+rsync --progress --compress root@iot.eclipse.org:~/logs/screen-1S-1.log ./cf.log
